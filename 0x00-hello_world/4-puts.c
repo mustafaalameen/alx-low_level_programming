bb0043b4ebbf597697b4277ce639c3fc0d800978
mustafaalameen <mustafaalameen2017@gmail.com>
2022-09-08 12:56:42 -0700

    Add 5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,13 @@
-puts("Programming is like building a multilingual puzzle)
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: It will be 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
