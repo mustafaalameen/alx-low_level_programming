448a4799c8480460195df9e40d72005f9eb32384
mustafaalameen <mustafaalameen2017@gmail.com>
2022-09-08 17:18:38 -0700

    Add 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 sucess
+ */
+
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-1");
+	return (1);
+}
