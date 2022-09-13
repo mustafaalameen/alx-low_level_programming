03d560a9e73b2b71981240e862b662f4c6d0d72c
mustafaalameen <mustafaalameen2017@gmail.com>
2022-09-13 05:16:05 -0700

    Add 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * print_sign - print ('+', '-', '0') if a certain condition is met
+ * @n: integer value to be checked
+ *
+ *Return: 1 when value is positive
+ *	 -1 when value is negative
+ *	  0 when value is zero
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
