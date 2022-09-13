0954440891d866bfd4b6844ab430946b34f6dcb3
mustafaalameen <mustafaalameen2017@gmail.com>
2022-09-13 02:50:30 -0700

    Add 2nd task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet - prints all alphabet in lowercase
+ *
+ * Description: The print_alphabet function prints all alphabet from a-z
+ * in lower case.
+ * Return: 0 to exit successfully
+ */
+
+void print_alphabet(void)
+{
+	char chr;
+
+	chr = 'a';
+
+	while (chr <= 'z')
+	{
+		putchar(chr);
+		chr++;
+	}
+}
