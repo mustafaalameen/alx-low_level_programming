3cdd55a779f883547de86206cb64c64af56686d7
mustafaalameen <mustafaalameen2017@gmail.com>
2022-09-13 09:44:06 -0700

    Add 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include "main.h"
 /**
  * main - Entry point
  *
@@ -8,7 +8,16 @@
 
 int main(void)
 {
-	printf("_putchar\n");
+	char pv[] =  "_putchar";
+
+	int i;
+
+	for (i = 0, i <= 9, i++)
+	{
+		_putchar(pv[i]);
+	}
+
+	_putchar('\n');
 
 	return (0);
 }
