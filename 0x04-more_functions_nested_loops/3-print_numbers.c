e963403187dd8a2a57b84ad44a124672e7bb90c2
mustafaalameen <mustafaalameen2017@gmail.com>
2022-09-15 09:24:04 -0700

    Add 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* print_numbers - prints value from 1 to 10
+*
+* Return: no value
+*/
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
