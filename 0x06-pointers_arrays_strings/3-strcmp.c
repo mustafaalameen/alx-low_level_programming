c05a074da26e4d969082b452e041f90ca2246678
Mustafa Al-ameen Mofeyisayo <mustafaalameen2017@gmail.com>
2022-09-23 03:40:13 +0100

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,11 +7,21 @@
  * @b: the second pointer to an integer
  */
 
-void swap_int(int *a, int *b)
-{
-	int temp;
+/**
+ * _strcmp - Compare two strings
+ * @s1: string
+ * @s2: string
+ * Return: negative int if s1 < s2, 0 if matching, and positive int if s1 > s2
+ */
 
-	temp = *a;
-	*a = *b;
-	*b = temp;
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	}
+	return (0);
 }
