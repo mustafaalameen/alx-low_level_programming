3c58dac43a1ae60dd9216fc0d2d773b09322a071
Mustafa Al-ameen Mofeyisayo <mustafaalameen2017@gmail.com>
2022-09-23 03:50:19 +0100

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,16 +2,28 @@
 #include "main.h"
 
 /**
- * swap_int - Swaps two values
- * @a: the first pointer to an integer
- * @b: the second pointer to an integer
+ * leet - encode a string into 1337
+ * @s: string
+ * Return: encoded string `s`
  */
 
-void swap_int(int *a, int *b)
+char *leet(char *s)
 {
-	int temp;
+	int i;
 
-	temp = *a;
-	*a = *b;
-	*b = temp;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		while (s[i] == 'a' || s[i] == 'A')
+			s[i] = '4';
+		while (s[i] == 'e' || s[i] == 'E')
+			s[i] = '3';
+		while (s[i] == 'o' || s[i] == 'O')
+			s[i] = '0';
+		while (s[i] == 't' || s[i] == 'T')
+			s[i] = '7';
+		while (s[i] == 'l' || s[i] == 'L')
+			s[i] = '1';
+	}
+
+	return (s);
 }
